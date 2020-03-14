18fc41b4e060dc06439d0e465177fd535b887610
Kizito Njoku <nkizito57@gmail.com>
2020-03-14 04:30:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+
+function map(array, callBack) {
+  let newArray = []
+  for (let i =0 ; i < array.length; i++) {
+  let ele =  array[i]
+    newArray.push(callBack(ele))
+  }
+  return newArray
+}
+
+
+
+const reduce = (array, callBack, startingpoint) => {
+   let tr = (!!startingpoint) ? startingpoint : array[0]
+  let i = (!!startingpoint) ? 0 : 1
+
+  for (; i < array.length; i++) {
+    tr = callBack(array[i], tr)
+  }
+
+  return tr;
+}
